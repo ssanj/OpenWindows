68955f6f58d1d7df69b86d0ab62a30d76913425a
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-08-04 20:42:14 +1000

    Add more detail to drop list

--- open_windows.py
+++ open_windows.py
@@ -25,7 +25,13 @@ class OpenWindowsCommand(sublime_plugin.WindowCommand):
       sublime.message_dialog("No active window found")
 
   def to_quick_panel_items(self, window_detail: WindowDetail) -> sublime.QuickPanelItem:
-    return sublime.QuickPanelItem(window_detail.folder, "", "", sublime.KIND_AMBIGUOUS)
+    details: List[str] = \
+      [
+        f"folder: {window_detail.folder}",
+        f"active view: {window_detail.active_view}"
+      ]
+    annotation = f"views({str(window_detail.view_count)})"
+    return sublime.QuickPanelItem(window_detail.short_name, details, annotation, sublime.KIND_AMBIGUOUS)
 
   def on_select(self, open_windows: List[WindowDetail] ,index: int) -> None:
     if index >= 0 and len(open_windows) > index:
--- window_detail.py
+++ window_detail.py
@@ -1,6 +1,26 @@
 import sublime
-from typing import NamedTuple
+from typing import NamedTuple, Optional
+import os.path
 
-class WindowDetail(NamedTuple):
-  window: sublime.Window
-  folder: str
+class WindowDetail:
+
+  def __init__(self, window: sublime.Window, folder: str) -> None:
+    self.window = window
+    self.folder = folder
+    self.short_name = os.path.basename(self.folder)
+    self.active_view = self.get_active_view()
+    self.view_count = len(window.views())
+
+  def get_active_view(self) -> str:
+    view: Optional[sublime.View] = self.window.active_view()
+    if view:
+      if (file_name := view.file_name()) is not None:
+        return file_name
+      elif view.name():
+        return view.name()
+      else:
+        return "-"
+    else:
+      return "<no active view>"
+
+
